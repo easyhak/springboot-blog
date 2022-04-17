862c3d841cfd90aff49ab355edc42d70b4db38d1
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2022-04-18 00:22:26 +0900

    PageSelect

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,19 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입 DI
 	private UserRepository userRepository;
 	
+	// http://localhost:8000/blog/dummy/users
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건의 데이터를 리턴받음
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUsers =  userRepository.findAll(pageable);
+		List<User> users = pagingUsers.getContent();
+		return users;
+	}
 	// {id} 주소로 파라미터를 전달받을 수 있음
 	// http://localhost:8000/blog/dummy/user/3
 	@GetMapping("/dummy/user/{id}")
@@ -29,6 +47,8 @@ public class DummyControllerTest {
 				return new IllegalArgumentException("해당 유저는 없습니다.id: "+ id);
 			}
 		}); // 반환 타입 Optional
+		
+		// user 객체 자바 object
 		return user;
 	}
 	// http://localhost:8000/blog/dummy/join (요청)
