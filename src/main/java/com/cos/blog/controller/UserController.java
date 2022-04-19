d671b1caa35c592eb73c3979c79082d706d2fe7b
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2022-04-19 23:13:38 +0900

    Create login, join page

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -6,5 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 	
+	@GetMapping("/user/joinForm")
+	public String joinForm() {
+		
+		return "user/joinForm";
+	}
 	
+	@GetMapping("/user/loginForm")
+	public String loginForm() {
+		
+		return "user/loginForm";
+	}
 }
