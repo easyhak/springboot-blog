32fdc9114dedb22d56d3a2e829b98f975290c02e
DESKTOP-GU49B66\이지학 <jooin2000@naver.com>
2022-04-20 22:54:12 +0900

    ajax function

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -14,7 +14,22 @@ let index = {
 		}
 		// console.log(data);
 		
-		$.ajax().done().fail(); // ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청을 한다.
+		// ajax호출시 default 비동기 호출
+		// ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청을 한다.
+		$.ajax({
+			//회원가입수행요청		
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // json 문자열 변경 // http body데이터
+			contentType : "application/json; charset=utf-8", // body데이터가 어떤 타입인지 (mime)
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript 오브젝트로 변경해줌 
+			// dataType 지정 안해도 됨
+		}).done(function(resp){
+			
+			location.href = "/blog";
+		}).fail(function(error){
+			alert(JSON.stringify(error))
+		});
 	}
 }
 
